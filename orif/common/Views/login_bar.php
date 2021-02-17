c72c9a399094f068febe121eda067a48280604f0
HeMa <MaximeEugene.Herbez@formation.orif.ch>
2021-02-17 12:59:51 +0100

    change of language file

--- orif/common/Views/login_bar.php
+++ orif/common/Views/login_bar.php
@@ -7,13 +7,13 @@ if (ENVIRONMENT !== 'testing') {
     //$config=config("\Modules\user\MY_user_config");
 //$CI->config->load('user/MY_user_config');
 
-    /**le fichier user/MY_user_lang.php se situe dans la version 4 dans le dossier /system */
+    /**le fichier user/common_lang.php se situe dans la version 4 dans le dossier /system */
 //$CI->lang->load('user/MY_user');
 } else {
 	// CI-PHPUnit checks from application/folder instead of module/folder
     //$config=config("\Modules\user\MY_user_config");
 //	$CI->config->load('../modules/user/config/MY_user_config');
-    /**le fichier user/MY_user_lang.php se situe dans la version 4 dans le dossier /system */
+    /**le fichier user/common_lang.php se situe dans la version 4 dans le dossier /system */
 //	$CI->lang->load('../../modules/user/language/fr/MY_user');
 
 }
@@ -26,7 +26,7 @@ if (ENVIRONMENT !== 'testing') {
       <a href="<?php echo base_url(); ?>" ><img src="<?php echo base_url("/assets/images/logo.png"); ?>" ></a>
     </div>
     <div class="col-sm-6">
-      <a href="<?php echo base_url(); ?>" class="text-info"><h1><?php echo lang('MY_application_lang.app_title'); ?></h1></a>
+      <a href="<?php echo base_url(); ?>" class="text-info"><h1><?php echo lang('common_lang.app_title'); ?></h1></a>
     </div>
     <div class="col-sm-3" >
       <div class="nav flex-column">
@@ -34,18 +34,18 @@ if (ENVIRONMENT !== 'testing') {
           
           <!-- ADMIN ACCESS ONLY -->
           <?php if ($_SESSION['user_access'] >= $config->item('access_lvl_admin')) { ?>
-              <a href="<?php echo base_url("user/admin/list_user"); ?>" ><?php echo lang('MY_user_lang.btn_admin'); ?></a><br />
+              <a href="<?php echo base_url("user/admin/list_user"); ?>" ><?php echo lang('common_lang.btn_admin'); ?></a><br />
           <?php } ?>
           <!-- END OF ADMIN ACCESS -->
 
           <!-- Logged in, display a "change password" button -->
-          <a href="<?php echo base_url("user/auth/change_password"); ?>" ><?php echo lang('MY_user_lang.btn_change_my_password'); ?></a>
+          <a href="<?php echo base_url("user/auth/change_password"); ?>" ><?php echo lang('common_lang.btn_change_my_password'); ?></a>
           <!-- and a "logout" button -->
-          <a href="<?php echo base_url("user/auth/logout"); ?>" ><?php echo lang('MY_user_lang.btn_logout'); ?></a><br />
+          <a href="<?php echo base_url("user/auth/logout"); ?>" ><?php echo lang('common_lang..btn_logout'); ?></a><br />
 
         <?php } else { ?>
           <!-- Not logged in, display a "login" button -->
-          <a href="<?php echo base_url("user/auth/login"); ?>" ><?php echo lang('MY_user_lang.btn_login'); ?></a>
+          <a href="<?php echo base_url("user/auth/login"); ?>" ><?php echo lang('common_lang.btn_login'); ?></a>
         <?php } ?>
       </div>
     </div>
