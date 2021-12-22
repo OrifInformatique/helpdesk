10580133d96033650da2598faf89a0da232f82b9
Simão Romano Schindler <simao.romanoschindler@formation.orif.ch>
2021-12-22 14:16:14 +0100

    added validation rules for password and password confirmation

--- orif/user/Models/User_model.php
+++ orif/user/Models/User_model.php
@@ -29,6 +29,16 @@ class User_model extends \CodeIgniter\Model{
                 'min_length['.config('\User\Config\UserConfig')->username_min_length.']|'.
                 'max_length['.config('\User\Config\UserConfig')->username_max_length.']'],
             'fk_user_type'=>['label'=>lang('user_lang.field_usertype'),'rules'=>'required|cb_not_null_user_type'],
+            'user_password'=>['label'=>lang('user_lang.field_password'),
+                'rules'=>'required|trim|'
+                .'min_length['.config("\User\Config\UserConfig")->password_min_length.']|'
+                .'max_length['.config("\User\Config\UserConfig")->password_max_length.']',],
+            'user_password_again'=>['label'=>lang('user_lang.field_password_confirm'),
+                'rules' =>'trim|required|'
+                .'min_length['.config("\User\Config\UserConfig")->password_min_length.']|'
+                .'max_length['.config("\User\Config\UserConfig")->password_max_length.']|'
+                .'matches[user_password]', 'errors' => [
+                    'matches'=>lang('Validation.matches', [lang('user_lang.field_password'), lang('user_lang.field_password_confirm')])]],
             'email'=>'cb_unique_useremail[{id}]|required|valid_email|max_length['.config("\User\Config\UserConfig")->email_max_length.']'];
         $this->validationMessages=[
             'id'=>['cb_not_null_user' => lang('user_lang.msg_err_user_not_exist')],
