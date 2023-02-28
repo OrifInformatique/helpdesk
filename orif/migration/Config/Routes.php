9837f7468674e48d3b6bfd74e57947764f22ee7d
Didier Viret <didier.viret@orif.ch>
2023-02-28 22:05:01 +0100

    Empty master branch

--- orif/migration/Config/Routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-$routes->get('migration','\Migration\Controllers\Migration::index');
-$routes->add('migration/(:any)','\Migration\Controllers\Migration::$1');
-$routes->post('migration/authenticate','\Migration\Controllers\AuthenticateMigration::authenticate');
-
-?>
