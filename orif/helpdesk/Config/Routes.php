0a4648f63fe91cf9ec0e212b0b4713b8c7169f37
Didier Viret <didier.viret@orif.ch>
2023-11-29 08:30:21 +0100

    Empty the "with composer" branch

--- orif/helpdesk/Config/Routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$routes->group('helpdesk',function($routes){
-    $routes->add('home','\Helpdesk\Controllers\Home');
-    $routes->add('home/(:any)','\Helpdesk\Controllers\Home::$1');
-    $routes->add('presence', '\Helpdesk\Controllers\Home::presence', ['filter' => 'filterAuth']);
-});
-
-?>
